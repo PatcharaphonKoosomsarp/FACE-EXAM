f0beca5c4e3fab20e7044c050b3cb75ba240c872
PatcharaphonKoosomsarp <s6506022620095@email.kmutnb.ac.th>
2025-12-01 21:42:29 +0700

    update

--- components/FaceVerification.tsx
+++ components/FaceVerification.tsx
@@ -378,7 +378,7 @@ const FaceVerification: React.FC<FaceVerificationProps> = ({ user, exam, onVerif
                 const labeledDescriptor = new faceapi.LabeledFaceDescriptors(user.id, descriptors);
                 setLabeledDescriptors([labeledDescriptor]);
                 // Use threshold 0.6 for FaceMatcher, but we will check distance manually too
-                setFaceMatcher(new faceapi.FaceMatcher([labeledDescriptor], 0.6));
+                setFaceMatcher(new faceapi.FaceMatcher([labeledDescriptor], 0.45));
                 setStatus('SCANNING');
                 startCamera();
 
