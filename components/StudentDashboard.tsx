037e3ca800257739197ee515a26f3c59c492e341
PatcharaphonKoosomsarp <s6506022620095@email.kmutnb.ac.th>
2026-01-26 14:43:50 +0700

    update

--- components/StudentDashboard.tsx
+++ components/StudentDashboard.tsx
@@ -173,7 +173,7 @@ const StudentDashboard: React.FC<StudentDashboardProps> = ({ user, exams, rooms,
               </div>
 
                   <div>
-                      <h2 className="text-xl font-bold text-gray-800 mb-4 flex items-center"><Calendar className="w-5 h-5 mr-2"/> ตารางสอบของคุณ</h2>
+                      <h2 className="text-xl font-bold text-gray-800 mb-4 flex items-center"><Calendar className="w-5 h-5 mr-2"/> รายการตารางสอบ</h2>
                       {exams.length === 0 ? (
                           <p className="text-gray-500 bg-gray-50 p-8 rounded-lg text-center">ยังไม่มีตารางสอบในขณะนี้</p>
                       ) : (
